2c0d46d30394246750afc8e34076660f8fee6c3e
Kian Wheeler <79857505+commander1228@users.noreply.github.com>
2026-01-28 23:22:28 -0500

    Added Allowed origins
    should now only accept localhost 3000 dev environment and mobiles apps
    
    only get & post are allowed

--- src/index.ts
+++ src/index.ts
@@ -9,26 +9,44 @@ import { setupSocket } from "./websocket/setupSocket";
 
 dotenv.config();
 
+
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+
+const allowedOrigins = (process.env.CORS_ORIGINS || "http://localhost:3000").split(",").map(origin => origin.trim());
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/chatroom", chatRoomRoutes);
 
 app.get("/", (_req, res) => {
-  res.json({ status: "Proximity API running" });
+  res.json({ status: "Proxima API running" });
 });
 
 const httpServer = createServer(app);
 
+
 const io = new Server(httpServer, {
   cors: {
-    origin: "*", // Adjust as needed for security
-    methods: ["GET", "POST"]
-  }
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
 setupSocket(io);
 
